694bba278226f6ffe2d2cba90c79232e39551414
Lyon <zhiyueonline2023@gmail.com>
2025-06-08 08:53:33 -0700

    Init Push

---
+++ src/main/java/com/qiuzhitech/onlineshopping_07/OnlineShopping07Application.java
@@ -0,0 +1,13 @@
+package com.qiuzhitech.onlineshopping_07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopping07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopping07Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/qiuzhitech/onlineshopping_07/OnlineShopping07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiuzhitech.onlineshopping_07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopping07ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
